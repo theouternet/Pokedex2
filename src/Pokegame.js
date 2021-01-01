d4ba383510d3fc43437fdb37cffa56944f89abd6
theouternet <jrougeau@gmail.com>
2021-01-01 15:43:49 -0600

    Pokegame stub

---
+++ src/Pokegame.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import App from "./App.js"
+import "Pokegame.css"
+
+//props
+
+class Pokegame extends Component {
+    render() {
+        //logic
+        return (
+            <div className="Pokegame">
+                <h1>Pokegame</h1>
+            </div>
+        )
+    }
+}
